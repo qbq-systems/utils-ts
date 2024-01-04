86e5fa60478191aeb29dedc1f5ee1fe8f5545034
Vladimir Galunshchikov <soyayaos@gmail.com>
2024-01-04 22:45:42 +0100

    added: `rand` `int` function

--- src/rand.ts
+++ src/rand.ts
@@ -1,3 +1,7 @@
+export const int = (f: number = 1, t: number = 100): number => {
+    return Math.floor(Math.random() * (t - f + 1) + f)
+}
+
 export const string = (length: number = 32): string => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     let result = ''
