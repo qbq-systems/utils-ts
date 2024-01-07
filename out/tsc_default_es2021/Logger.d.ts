e7d0738b56edfff9b730f68fde08ad53d39d9495
Vladimir Galunshchikov <soyayaos@gmail.com>
2024-01-07 13:02:13 +0100

    changed `Logger`: added new helpers

--- out/tsc_default_es2021/Logger.d.ts
+++ out/tsc_default_es2021/Logger.d.ts
@@ -1,5 +1,9 @@
 export default class Logger {
+    private prefix;
+    private useUTC;
+    addPrefix(p: string): this;
     info(...m: any[]): void;
     private process;
+    setPrefix(...p: any[]): this;
 }
 //# sourceMappingURL=Logger.d.ts.map
--- src/Logger.ts
+++ src/Logger.ts
@@ -1,11 +1,32 @@
+import { utc } from "./time";
+
 export default class Logger {
+
+    private prefix: string[] = []
+
+    private useUTC: boolean = true
+
+    addPrefix(p: string) {
+        this.prefix.push(p)
+        return this
+    }
 
     info(...m: any[]) {
         this.process('info', m)
     }
 
     private process(type: string, m: any[]) {
-        console.log(`[${type}]`, m.join(' '))
+        console.log(
+            this.useUTC ? utc() : '',
+            `[${type}]`,
+            this.prefix.length > 0 ? `[${this.prefix.join('/')}]` : '',
+            m.join(' '),
+        )
+    }
+
+    setPrefix(...p: any[]) {
+        this.prefix = p
+        return this
     }
 
 }
